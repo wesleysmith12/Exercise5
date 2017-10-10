80903fcec09581a4b1d3472c3af77d5e2e6408d5
Wesley <wesleysmith1@mail.weber.edu>
2017-10-10 08:50:00 -0600

    Initial commit

---
+++ Exercise5/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="container">
+        <app-header></app-header>
+        <hr>
+        <div class="row">
+            <app-server></app-server>
+            <app-server-details></app-server-details>
+        </div>
+        <hr>
+        <app-footer></app-footer>
+    </div>
+</template>
+
+<script>
+    import Header from './Header.vue'
+    import Footer from './Footer.vue'
+    import Server from './Server.vue'
+    import ServerDetails from './ServerDetails.vue'
+
+    export default {
+        components: {
+            'app-header': Header,
+            'app-footer': Footer,
+            'app-server': Server,
+            'app-server-details': ServerDetails
+        }
+    }
+</script>
+
+<style>
+
+</style>
